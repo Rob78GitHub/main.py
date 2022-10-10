7796db208e72361e31a8db797574b6ae99de2ad3
Rob78GitHub <80572037+Rob78GitHub@users.noreply.github.com>
2022-10-10 23:24:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print('Hallo Welt')
+print('1')
+print(5*10+3)
+name=input('Bitte Namen eingeben: ')
+
+#name = 'robert'
+alter =43
+print(name+" "+str(33))
+mybool=True
+
+if(name=='Robert'):
+
+    print('true!')
+    for x in range(1, 100, 1):
+        print(x)
+else:
+    print('false!')
+print()
+input('')
