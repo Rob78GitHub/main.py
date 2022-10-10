da767f191c8640be0de6967b641af400ae408ed5
Rob78GitHub <80572037+Rob78GitHub@users.noreply.github.com>
2022-10-10 23:28:28 +0200

    ok

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 print('Hallo Welt')
 print('1')
-print(5*10+3)
-name=input('Bitte Namen eingeben: ')
+print(5*10+3.4)
+name = input('Bitte Namen eingeben: ')
 
 #name = 'robert'
-alter =43
+alter = 43
 print(name+" "+str(33))
 mybool=True
 
-if(name=='Robert'):
+if(name=="Robert"):
 
     print('true!')
     for x in range(1, 100, 1):
